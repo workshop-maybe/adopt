4a3dfafc9ba9cae335503e45c4090d50dc715ec6
James Dunseith <info@workshopmaybe.com>
2021-02-16 18:45:08 -0500

    Incorporate OCG updates and merge conflicts

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import Link from 'next/link'
-<<<<<<< HEAD
 import styles from '../styles/Home.module.css'
 import { getStylePlaygroundPages } from '../lib/stylePlaygroundList'
+import Layout from '../components/layout'
 
 export async function getStaticProps() {
   const getStyleExamples = getStylePlaygroundPages()
@@ -16,9 +16,6 @@ export async function getStaticProps() {
 export default function Home({ getStyleExamples }) {
 
   console.log(getStyleExamples)
-=======
-import Layout from '../components/layout'
->>>>>>> 94ffb3f9254c07524a39db3b5079ba32b49b3e8a
 
   return (
     <Layout>
@@ -30,7 +27,6 @@ import Layout from '../components/layout'
         <h1>Hello Adopt</h1>
         <Link href='/about'>About Page</Link>
 
-<<<<<<< HEAD
       <h2>Style Playground (long way)</h2>
       <Link href='/styleplayground/field'>Field</Link>
       <Link href='/styleplayground/ryan'>Ryan</Link>
@@ -53,25 +49,6 @@ import Layout from '../components/layout'
           
         ))}
       </ul>
-    </div>
-=======
-        <h2>Style Playground</h2>
-        <Link href='/styleplayground/field'>Field</Link>
-        <Link href='/styleplayground/ryan'>Ryan</Link>
-        <Link href='/styleplayground/brayden'>Brayden</Link>
-        <Link href='/styleplayground/juan'>Juan</Link>
-        <Link href='/styleplayground/eric'>Eric</Link>
-        <Link href='/styleplayground/cole'>Cole</Link>
-        <Link href='/styleplayground/dan'>Dan</Link>
-        <Link href='/styleplayground/quasar'>Quasar</Link>
-        <Link href='/styleplayground/a'>A</Link>
-        <Link href='/styleplayground/kyle'>Kyle</Link>
-        <Link href='/styleplayground/colin'>Colin</Link>
-        <Link href='/styleplayground/james'>James</Link>
-        <Link href='/styleplayground/ocg'>OCG</Link>
-
-
     </Layout>
->>>>>>> 94ffb3f9254c07524a39db3b5079ba32b49b3e8a
   )
 }
