c78a7d0eb62cee1517d90d1d6966c9128f2be659
James Dunseith <info@workshopmaybe.com>
2021-02-15 20:31:52 -0500

    Add my style examples

--- pages/styleplayground/james.js
+++ pages/styleplayground/james.js
@@ -5,10 +5,25 @@ function james() {
     return (
         <div>
             <h1>James Style Experiments</h1>
-            <div className={styles.shape}>Make me a different shape</div>
-            <div className={styles.animate}>Animate me!</div> 
-            <div className={styles.shadows}>Apply some shadows to me</div>
-            <div className={styles.textdecorated}>Decorate my text</div>
+            <div className={styles.shape}>
+                <p>
+                    I'm a new shape!
+                </p>
+            </div>
+            <p>
+                <a href='https://bennettfeely.com/clippy/'>
+                    CSS clip-path maker
+                </a>
+            </p>
+            <div className={styles.movement}>can't stop won't stop</div>
+            <div className={styles.shadow}>what's in the shade there?</div>
+            <div className={styles.text}>
+                <p>James</p>
+                <p><a href='https://fonts.google.com/' className={styles.jameslinks}>Google Fonts</a></p>
+            </div>
+            <div className={styles.text}>
+                <a href='https://cssgradient.io/'>cool tool click here</a>
+            </div>
         </div>
     )
 }
